91512a3dbe10204fbbd5ce17b5b5ad22d5a9bf21
Sanchay srijan <sanchaysrijans@Technoidentity.com>
2026-09-10 23:59:17 +0530

    Add basic cargo file and run

---
+++ Week-2/Day-2/projects/hello_cargo/target/debug/deps/hello_cargo-91f5a05e9906e1e6.d
@@ -0,0 +1,5 @@
+/home/technoidentity/Desktop/Learning and Development/Week-2/Day-2/projects/hello_cargo/target/debug/deps/hello_cargo-91f5a05e9906e1e6.d: src/main.rs
+
+/home/technoidentity/Desktop/Learning and Development/Week-2/Day-2/projects/hello_cargo/target/debug/deps/hello_cargo-91f5a05e9906e1e6: src/main.rs
+
+src/main.rs:
