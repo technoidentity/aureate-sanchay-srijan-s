f6edfe3596f4a63d6143d79891e56f59a86a79b0
Sanchay srijan <sanchaysrijans@Technoidentity.com>
2026-09-11 00:33:29 +0530

    add comments for reference

--- Week-2/Day-2/projects/guessing_game/src/main.rs
+++ Week-2/Day-2/projects/guessing_game/src/main.rs
@@ -1,8 +1,10 @@
 use std::io;
-
+// input a number and it return the number thats all
+// takes in input using std::io::stdin()
+// variable is immutable by default hence the mut in the variable.
 fn main() {
-    println!("Welcome to guessing game");
-    println!("Hey buddy ,Guess a number ");
+    println!("Welcome to guessing game sanchay ,i will erturn it to you !!");
+    println!("Guess a number now,:");
 
     let mut guess = String::new();
 
