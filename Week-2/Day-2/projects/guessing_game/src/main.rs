d5daa06b58f844ad90ef4ec1126c4601aa3fcbf8
Sanchay srijan <sanchaysrijans@Technoidentity.com>
2026-09-11 12:20:08 +0530

    Program generate random number using rand::RngExt::rand::rng().random_range(1..=100); and user Predicts it using while loop and if ,else statements.

--- Week-2/Day-2/projects/guessing_game/src/main.rs
+++ Week-2/Day-2/projects/guessing_game/src/main.rs
@@ -1,16 +1,40 @@
 use std::io;
+use rand::RngExt;
+
 // input a number and it return the number thats all
 // takes in input using std::io::stdin()
 // variable is immutable by default hence the mut in the variable.
 fn main() {
     println!("Welcome to guessing game sanchay ,i will erturn it to you !!");
-    println!("Guess a number now,:");
+    let secret_number = rand::rng().random_range(1..=100);
+    println!("Guess a number now:");
 
-    let mut guess = String::new();
+    let mut guess = 0;
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("the read_line failed");
+    while guess != secret_number{
 
-    println!("you guessed the number {guess}");
+        let mut input  = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("the read_line failed");
+
+        guess = input
+                   .trim()
+                   .parse()
+                   .expect("Please enter a number");
+
+        if secret_number > guess {
+            println!("your guessed number is lower than the secret_number");
+        }
+        else if secret_number < guess {
+            println!("your guessed number is higher than the secret_number");
+        }
+        else {
+            println!("Boooyahhhh .... you guessed the number {guess} and its right , Let's gooo..");
+            break;
+        }
+    }
+
+
+
 }
---
+++ Week-2/Day-2/projects/guessing_game/target/debug/deps/cpufeatures-26ba7571a322b621.d
@@ -0,0 +1,9 @@
+/home/technoidentity/Desktop/Learning and Development/Week-2/Day-2/projects/guessing_game/target/debug/deps/cpufeatures-26ba7571a322b621.d: /home/technoidentity/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cpufeatures-0.3.1/src/lib.rs /home/technoidentity/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cpufeatures-0.3.1/src/x86.rs /home/technoidentity/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cpufeatures-0.3.1/src/../README.md
+
+/home/technoidentity/Desktop/Learning and Development/Week-2/Day-2/projects/guessing_game/target/debug/deps/libcpufeatures-26ba7571a322b621.rlib: /home/technoidentity/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cpufeatures-0.3.1/src/lib.rs /home/technoidentity/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cpufeatures-0.3.1/src/x86.rs /home/technoidentity/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cpufeatures-0.3.1/src/../README.md
+
+/home/technoidentity/Desktop/Learning and Development/Week-2/Day-2/projects/guessing_game/target/debug/deps/libcpufeatures-26ba7571a322b621.rmeta: /home/technoidentity/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cpufeatures-0.3.1/src/lib.rs /home/technoidentity/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cpufeatures-0.3.1/src/x86.rs /home/technoidentity/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cpufeatures-0.3.1/src/../README.md
+
+/home/technoidentity/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cpufeatures-0.3.1/src/lib.rs:
+/home/technoidentity/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cpufeatures-0.3.1/src/x86.rs:
+/home/technoidentity/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cpufeatures-0.3.1/src/../README.md:
